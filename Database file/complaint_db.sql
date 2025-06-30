28617e9ff532932e6dd0edbb422c20903f7f0387
icebergadilmoin <104529937+icebergadilmoin@users.noreply.github.com>
2025-06-30 21:22:13 +0500

    Add files via upload

---
+++ Database file/complaint_db.sql
@@ -0,0 +1,353 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 30, 2025 at 06:04 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `complaint_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `assignments`
+--
+
+CREATE TABLE `assignments` (
+  `id` int(11) NOT NULL,
+  `complaint_id` int(11) NOT NULL,
+  `worker_id` int(11) NOT NULL,
+  `assigned_by` int(11) NOT NULL,
+  `assigned_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `status` enum('Pending','Accepted','In Progress','Completed','Rejected') DEFAULT 'Pending',
+  `progress_notes` text DEFAULT NULL,
+  `last_updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `assignments`
+--
+
+INSERT INTO `assignments` (`id`, `complaint_id`, `worker_id`, `assigned_by`, `assigned_at`, `status`, `progress_notes`, `last_updated`) VALUES
+(1, 1, 3, 1, '2025-06-27 21:43:03', 'Pending', NULL, '2025-06-29 07:17:09');
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `assignment_progress`
+-- (See below for the actual view)
+--
+CREATE TABLE `assignment_progress` (
+`complaint_id` int(11)
+,`workers_assigned` bigint(21)
+,`workers_completed` decimal(23,0)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `complaints`
+--
+
+CREATE TABLE `complaints` (
+  `id` int(11) NOT NULL,
+  `client_id` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `description` text NOT NULL,
+  `type` enum('CCTV','Solar','Other') NOT NULL,
+  `status` enum('Pending','Assigned','In Progress','Resolved') DEFAULT 'Pending',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `version` int(11) DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `complaints`
+--
+
+INSERT INTO `complaints` (`id`, `client_id`, `title`, `description`, `type`, `status`, `created_at`, `version`) VALUES
+(1, 2, 'camera number 4 is not working', 'dha phase 4 house number 59dd my camera number 4 is not working', 'CCTV', 'In Progress', '2025-06-27 20:03:58', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `complaint_media`
+--
+
+CREATE TABLE `complaint_media` (
+  `id` int(11) NOT NULL,
+  `complaint_id` int(11) NOT NULL,
+  `uploader_id` int(11) NOT NULL,
+  `file_path` varchar(255) NOT NULL,
+  `file_type` enum('image','video','document') DEFAULT NULL,
+  `annotations` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`annotations`))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `media`
+--
+
+CREATE TABLE `media` (
+  `id` int(11) NOT NULL,
+  `complaint_id` int(11) NOT NULL,
+  `file_path` varchar(255) NOT NULL,
+  `uploaded_by` int(11) NOT NULL,
+  `uploaded_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `media`
+--
+
+INSERT INTO `media` (`id`, `complaint_id`, `file_path`, `uploaded_by`, `uploaded_at`) VALUES
+(1, 1, '685ef92ea58c1_Capture.JPG', 2, '2025-06-27 20:03:58');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `id` int(11) NOT NULL,
+  `complaint_id` int(11) NOT NULL,
+  `sender_id` int(11) NOT NULL,
+  `message` text NOT NULL,
+  `sent_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `read_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `messages`
+--
+
+INSERT INTO `messages` (`id`, `complaint_id`, `sender_id`, `message`, `sent_at`, `read_at`) VALUES
+(1, 1, 2, 'hello', '2025-06-27 21:03:26', '2025-06-27 21:21:30'),
+(2, 1, 1, 'yes', '2025-06-27 21:21:30', '2025-06-27 21:43:51'),
+(3, 1, 1, 'hello', '2025-06-27 21:21:35', '2025-06-27 21:43:51'),
+(4, 1, 1, 'mr azeem will visit you', '2025-06-27 21:43:20', '2025-06-27 21:43:51'),
+(5, 1, 3, 'asslam-o-alaikum sir i am on the way', '2025-06-27 21:44:46', '2025-06-27 21:45:07'),
+(6, 1, 1, 'azeem ap kay pass 2 bajay ayega', '2025-06-27 21:51:46', '2025-06-27 21:52:06'),
+(7, 1, 3, 'main rasty main hun', '2025-06-27 21:52:49', '2025-06-29 07:05:57');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notifications`
+--
+
+CREATE TABLE `notifications` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `message` text NOT NULL,
+  `is_read` tinyint(1) DEFAULT 0,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('admin','manager','worker','client') NOT NULL,
+  `full_name` varchar(100) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `address` text DEFAULT NULL,
+  `specialization` varchar(100) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `role`, `full_name`, `phone`, `address`, `specialization`, `created_at`) VALUES
+(1, 'admin', 'admin@system.com', '$2y$10$EWyaERGvBr1PZ0Pxa84ih.aqV1FgFHmoE9oLCIqKYXOjzoDRjnP6O', 'admin', 'System Administrator', NULL, NULL, NULL, '2025-06-27 19:25:57'),
+(2, 'musabhai', 'musa@cms.com', '$2y$10$6SeFMdMAEa5.jQhUAg6Gu.c.ch.WvLpKsXBL6lTGFO18FPU9K52Ke', 'client', 'musa', '112233445566', '117p', NULL, '2025-06-27 19:51:37'),
+(3, 'azeemjaved', 'azeem@cms.com', '$2y$10$zpyiNzOgqAJ.pcBPlkbIWOUTp36TMqwlMIUX/iRblGsNaEeNZkf9e', 'worker', 'Azeem Javed', NULL, NULL, 'CCTV', '2025-06-27 20:11:55'),
+(4, 'umerzabih', 'Umar@cms.com', '$2y$10$mE5nVwWU2k7rF6zLpuose.qd46O70MZPoSIoa9dV42bE7YBR9GloW', 'manager', 'Umar Zabih', NULL, NULL, NULL, '2025-06-27 21:50:32');
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `assignment_progress`
+--
+DROP TABLE IF EXISTS `assignment_progress`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `assignment_progress`  AS SELECT `c`.`id` AS `complaint_id`, count(`a`.`id`) AS `workers_assigned`, sum(`a`.`status` = 'Completed') AS `workers_completed` FROM (`complaints` `c` left join `assignments` `a` on(`c`.`id` = `a`.`complaint_id`)) GROUP BY `c`.`id` ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `assignments`
+--
+ALTER TABLE `assignments`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `complaint_id` (`complaint_id`,`worker_id`),
+  ADD KEY `worker_id` (`worker_id`),
+  ADD KEY `assigned_by` (`assigned_by`);
+
+--
+-- Indexes for table `complaints`
+--
+ALTER TABLE `complaints`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `client_id` (`client_id`);
+
+--
+-- Indexes for table `complaint_media`
+--
+ALTER TABLE `complaint_media`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `complaint_id` (`complaint_id`),
+  ADD KEY `uploader_id` (`uploader_id`);
+
+--
+-- Indexes for table `media`
+--
+ALTER TABLE `media`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `complaint_id` (`complaint_id`),
+  ADD KEY `uploaded_by` (`uploaded_by`);
+
+--
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `complaint_id` (`complaint_id`),
+  ADD KEY `sender_id` (`sender_id`);
+
+--
+-- Indexes for table `notifications`
+--
+ALTER TABLE `notifications`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `assignments`
+--
+ALTER TABLE `assignments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `complaints`
+--
+ALTER TABLE `complaints`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `complaint_media`
+--
+ALTER TABLE `complaint_media`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `media`
+--
+ALTER TABLE `media`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `notifications`
+--
+ALTER TABLE `notifications`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `assignments`
+--
+ALTER TABLE `assignments`
+  ADD CONSTRAINT `assignments_ibfk_1` FOREIGN KEY (`complaint_id`) REFERENCES `complaints` (`id`),
+  ADD CONSTRAINT `assignments_ibfk_2` FOREIGN KEY (`worker_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `assignments_ibfk_3` FOREIGN KEY (`assigned_by`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `complaints`
+--
+ALTER TABLE `complaints`
+  ADD CONSTRAINT `complaints_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `complaint_media`
+--
+ALTER TABLE `complaint_media`
+  ADD CONSTRAINT `complaint_media_ibfk_1` FOREIGN KEY (`complaint_id`) REFERENCES `complaints` (`id`),
+  ADD CONSTRAINT `complaint_media_ibfk_2` FOREIGN KEY (`uploader_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `media`
+--
+ALTER TABLE `media`
+  ADD CONSTRAINT `media_ibfk_1` FOREIGN KEY (`complaint_id`) REFERENCES `complaints` (`id`),
+  ADD CONSTRAINT `media_ibfk_2` FOREIGN KEY (`uploaded_by`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `messages`
+--
+ALTER TABLE `messages`
+  ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`complaint_id`) REFERENCES `complaints` (`id`),
+  ADD CONSTRAINT `messages_ibfk_2` FOREIGN KEY (`sender_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `notifications`
+--
+ALTER TABLE `notifications`
+  ADD CONSTRAINT `notifications_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
